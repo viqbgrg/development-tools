ead093a1d09b5da328a36e044ebebf587743ecff
viqbgrg <viqbgrg@gmail.com>
2021-06-09 16:20:44 +0800

    idea http

---
+++ springboot-ideahttp/src/test/java/com/github/viqbgrg/springbootideahttp/SpringbootIdeahttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viqbgrg.springbootideahttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIdeahttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
